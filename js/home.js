7862a3079a04560c9b850d063dffae6bd2df2c58
nehalkhaled19 <nehalkhaled62@gmail.com>
2024-07-31 18:28:34 +0300

    responsive navbar

--- js/home.js
+++ js/home.js
@@ -1,3 +1,18 @@
+// Navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled')
+        navbar.classList.remove('static')
+        
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+        navbar.classList.add('static')
+    }
+})
+
+
+// Slider & Animation
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
     AOS.init({
