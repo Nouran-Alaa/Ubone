31f7453e976b3cd9f0a82c74c040ec94043cc7d5
nehalkhaled19 <nehalkhaled62@gmail.com>
2024-07-31 03:05:15 +0300

    home (html&css)

---
+++ js/home.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+    AOS.init({
+        once: true 
+    });
+
+  
+    setTimeout(function () {
+        $(".collection-btn").addClass("aos-animate");
+    }, 400); 
+  });
+
+  var owl = $('#slider1');
+  owl.owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true,
+      animateOut: 'fadeOut'
+  });  
+  $('#brands').owlCarousel({
+      items:5,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true
+  });
+ 
