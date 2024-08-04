78bbdfe45bfc51105b17206439ad4c0f9bc2ff93
Karim Bonda <132497131+KarimAdel-1@users.noreply.github.com>
2024-08-05 02:45:28 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -588,6 +588,18 @@ function toggleSidebar() {
   sidebar.classList.toggle("visible");
 }
 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled')
+        navbar.classList.remove('static')
+        
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+        navbar.classList.add('static')
+    }
+})
+
 // Initialize toggle button functionality
 function init() {
   displayProducts(currentPage);
