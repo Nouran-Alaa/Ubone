3b9fbd4f54feaa60c8d7e2251632e3ea56a89c92
Karim Bonda <132497131+KarimAdel-1@users.noreply.github.com>
2024-08-05 02:50:29 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -199,6 +199,35 @@ function displayProducts(page) {
     productGrid.appendChild(productElement);
   });
 }
+//// function to update counters
+function updateCartCounter() {
+  const cartCounterElement = document.querySelector(".cart-counter");
+  if (cartCounterElement) {
+    cartCounterElement.textContent = userCart.reduce(
+      (acc, curr) => acc + curr.quantity,
+      0
+    );
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  updateCartCounter();
+});
+
+function updateWishlistCounter() {
+  let userWishList = JSON.parse(sessionStorage.getItem("userWishList")) || [];
+  const wishlistCounterElement = document.querySelector(".wishlist-counter");
+  if (wishlistCounterElement) {
+    wishlistCounterElement.textContent = userWishList.reduce(
+      (acc, curr) => acc + curr.wishlist,
+      0
+    );
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  updateWishlistCounter();
+});
 
 // Function to get product by ID
 function getProductById(id) {
